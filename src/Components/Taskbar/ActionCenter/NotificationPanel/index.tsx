757d016ba432f13eea041878185273e27e88bf00
JustinL <93564256+hockerdiscord@users.noreply.github.com>
2021-12-28 20:40:15 +0700

    Update index.tsx

--- src/Components/Taskbar/ActionCenter/NotificationPanel/index.tsx
+++ src/Components/Taskbar/ActionCenter/NotificationPanel/index.tsx
@@ -35,7 +35,7 @@ export const NotificationPanel = () => {
       <div class={styles.sub_container}>
         <div class={styles.battery_indicator}>
           <BatteryIcon />
-          <p>69%</p>
+          <p>∞</p>
         </div>
         <div class={styles.other_icons}>
           <PencilIcon size="16" />
